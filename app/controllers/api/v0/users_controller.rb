e70b706615c974a88eb49d20e4ecfc9b3708842e
Grace Joh <graceehjoh@gmail.com>
2023-06-12 21:26:02 -0500

    refactor: UsersController#create with return error clause

--- app/controllers/api/v0/users_controller.rb
+++ app/controllers/api/v0/users_controller.rb
@@ -2,12 +2,10 @@ class Api::V0::UsersController < ApplicationController
   rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
   def create
-    if params[:password] == params[:password_confirmation]
-      user = User.create!(user_params)
-      render json: UsersSerializer.new(user), status: 201
-    else
-      render json: { error: 'Passwords do not match' }, status: 401
-    end
+    return render_unprocessable_content_response unless params[:password] == params[:password_confirmation]
+
+    user = User.create!(user_params)
+    render json: UsersSerializer.new(user), status: 201
   end
 
   private
@@ -18,5 +16,9 @@ class Api::V0::UsersController < ApplicationController
 
   def render_unprocessable_entity_response(exception)
     render json: { error: exception.message }, status: 422
+  end
+
+  def render_unprocessable_content_response
+    render json: { error: 'Passwords do not match' }, status: 401
   end
 end
