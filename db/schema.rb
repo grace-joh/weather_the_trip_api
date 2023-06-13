376ac0fc2be5dca8e703b88cd6c0c0543f8ce425
Grace Joh <graceehjoh@gmail.com>
2023-06-12 19:53:05 -0500

    refactor: add unique index to api_key

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_12_234717) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_13_004651) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_12_234717) do
     t.string "api_key"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["api_key"], name: "index_users_on_api_key", unique: true
   end
 
 end
