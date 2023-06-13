5e99da43c207f595c10c7034d5d74ff2b640b6ac
Grace Joh <graceehjoh@gmail.com>
2023-06-12 19:40:24 -0500

    feat/test: add api_key callback and test user creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
   has_secure_password
+
+  before_create do
+    self.api_key = SecureRandom.hex(16)
+  end
 end
