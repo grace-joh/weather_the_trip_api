db8aea530474e8815ad82114a80ece4a7a0310e9
Grace Joh <graceehjoh@gmail.com>
2023-06-12 19:00:19 -0500

    feat/test: create User model

---
+++ app/models/user.rb
@@ -0,0 +1,4 @@
+class User < ApplicationRecord
+  validates :email, presence: true, uniqueness: true
+  has_secure_password
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  it { should validate_presence_of :email }
+  it { should validate_uniqueness_of :email }
+  it { should have_secure_password }
+end
