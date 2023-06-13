315213b8885ed9c5f68385828654a90e71268ff2
Grace Joh <graceehjoh@gmail.com>
2023-06-12 22:17:49 -0500

    setup: add route for user login

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
     namespace :v0 do
       get '/forecast', to: 'forecast#search'
       post '/users', to: 'users#create'
+      post '/sessions', to: 'sessions#create'
     end
   end
 end
