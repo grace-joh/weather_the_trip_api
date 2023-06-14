934196bcf1b763899bfdb0d2197edb043c799262
Grace Joh <graceehjoh@gmail.com>
2023-06-13 19:15:53 -0500

    setup: create roadtrip creation route

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
       get '/forecast', to: 'forecast#search'
       post '/users', to: 'users#create'
       post '/sessions', to: 'sessions#create'
+      post '/road_trip', to: 'road_trip#create'
     end
   end
 end
